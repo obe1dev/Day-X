6b0e3a6bc7eca19d50d313132e7d8f73cc1d7b38
Brock Oberhansley <brockoberhansley@Brocks-MacBook-Pro.local>
2015-12-27 15:06:43 -0700

    add to firebase

---
+++ FirebaseController.h
@@ -0,0 +1,13 @@
+//
+//  FirebaseController.h
+//  DayX
+//
+//  Created by Brock Oberhansley on 10/17/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirebaseController : NSObject
+
+@end
---
+++ FirebaseController.m
@@ -0,0 +1,13 @@
+//
+//  FirebaseController.m
+//  DayX
+//
+//  Created by Brock Oberhansley on 10/17/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+#import "FirebaseController.h"
+
+@implementation FirebaseController
+
+@end
